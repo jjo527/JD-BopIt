1e0fe8bb056f294db4b9de2ea9d9ce7c54daf59a
JJ Oswald <jjo50@pitt.edu>
2023-06-21 11:23:12 -0400

    Added hello world files

---
+++ Hello_World/Assignment8-3lights.ino
@@ -0,0 +1,33 @@
+void setup() {
+  pinMode(7, INPUT);
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+
+  if(digitalRead(7) == HIGH) {
+    // blink bottom light
+    digitalWrite(2, HIGH);
+    delay(500);            
+    digitalWrite(2, LOW);  
+    delay(500);
+
+    // blink middle light
+    digitalWrite(1, HIGH);
+    delay(500);            
+    digitalWrite(1, LOW);  
+    delay(500);
+
+    // blink top light
+    digitalWrite(0, HIGH);
+    delay(500);            
+    digitalWrite(0, LOW);  
+    delay(500);    
+  }  
+}
