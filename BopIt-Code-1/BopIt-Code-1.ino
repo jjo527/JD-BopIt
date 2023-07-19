ab1a8e923c958ed114e07b620b747f4d8b30418b
JJ Oswald <jjo50@pitt.edu>
2023-07-19 12:02:52 -0400

    some epic functions

--- BopIt-Code-1/BopIt-Code-1.ino
+++ BopIt-Code-1/BopIt-Code-1.ino
@@ -1,15 +1,15 @@
 void setup() {
   pinMode(13, INPUT); // start/stop button input
-  pinMode(6, INPUT); // top/left input 
+  pinMode(6, INPUT); // top/left input
   pinMode(7, INPUT); // middle/middle input
   pinMode(8, INPUT); // bottom/right input
-  
+
   pinMode(0, OUTPUT); // reset counter (top light = fail)
   pinMode(1, OUTPUT); // increment counter (middle light = success)
 
   pinMode(2, OUTPUT); // bottom output light on right, not being used currently
 
-  //pinMode(9, OUTPUT); // bottom light signaling input 
+  //pinMode(9, OUTPUT); // bottom light signaling input
   //pinMode(10, OUTPUT); // middle light signaling input
   pinMode(11, OUTPUT); // BUZZER OUTPUT FOR SIGNALING INPUTS (used to be top light signaling input)
   //TCCR2B = TCCR2B & 0b11111000 | 0x01;
@@ -18,16 +18,16 @@ void setup() {
 
 }
 
-void loop() 
+void loop()
 {
-  bool isRunning = false;  
+  bool isRunning = false;
   digitalWrite(1, LOW);
   delay(500);
 
   // toggle start light and logic
   if(digitalRead(13) == HIGH) {
     digitalWrite(12, !digitalRead(12));
-    
+
     if (digitalRead(12) == HIGH) {
       isRunning = true;
       Serial.print("start stop indicator turned on");
@@ -38,18 +38,17 @@ void loop()
     }
 
     delay(5000);
-  }  
+  }
 
   // constant while loop that gives/checks for inputs
   while (isRunning) {
     int randNumber = random(1,4); //generates random command number
 
     int decreaseTime = 0;
-
     int score = 0;
-      
+
     if(randNumber == 1) {//first command check
-      
+
       //give command
       digitalWrite(11, HIGH);
       delay(100);
@@ -86,7 +85,7 @@ void loop()
           delay(500);
           userInput = true;
           break;
-        } 
+        }
       }
 
       //check score
@@ -101,7 +100,7 @@ void loop()
         delay(500);
 
       }
-      
+
       digitalWrite(11, LOW);
       delay(500);
 
@@ -121,32 +120,21 @@ void loop()
         delay(1);
 
         if (digitalRead(6) == HIGH && userInput == false) {
-          digitalWrite(0, HIGH);
-          delay(500);
-          digitalWrite(0, LOW);
-          delay(500);
+          bad_input();
           userInput = true;
           break;
         }
         else if (digitalRead(7) == HIGH && userInput == false) {
-          Serial.print("correct input detected");
-          digitalWrite(1, HIGH);
-          delay(500);
-          digitalWrite(1, LOW);
-          delay(500);
+          good_input();
           userInput = true;
           score += 1;
           break;
         }
         else if (digitalRead(8) == HIGH && userInput == false) {
-          digitalWrite(0, HIGH);
-          delay(500);
-          digitalWrite(0, LOW);
-          delay(500);
+          good_input();
           userInput = true;
           break;
-        } 
-
+        }
       }
 
       if (score >= 99){
@@ -159,7 +147,7 @@ void loop()
         digitalWrite(0, LOW);
         delay(500);
       }
-      
+
       digitalWrite(11, LOW);
       delay(500);
 
@@ -207,7 +195,7 @@ void loop()
           userInput = true;
           score += 1;
           break;
-        } 
+        }
 
       }
 
@@ -221,7 +209,7 @@ void loop()
         digitalWrite(0, LOW);
         delay(500);
       }
-      
+
       digitalWrite(11, LOW);
       delay(500);
 
@@ -239,7 +227,7 @@ bool check_for_start_button(bool isRunning)
 {
     if(digitalRead(13) == HIGH) {
       digitalWrite(12, !digitalRead(12));
-      
+
       if (digitalRead(12) == HIGH) {
         isRunning = true;
       }
@@ -247,6 +235,22 @@ bool check_for_start_button(bool isRunning)
         isRunning = false;
       }
       delay(1000);
-    }  
+    }
     return isRunning;
 }
+
+void bad_input() {
+  digitalWrite(0, HIGH);
+  delay(500);
+  digitalWrite(0, LOW);
+  delay(500);
+}
+
+void good_input() {
+  Serial.print("correct input detected");
+  digitalWrite(1, HIGH);
+  delay(500);
+  digitalWrite(1, LOW);
+  delay(500);
+}
+
